3ad66f5230e2d231446a32e53e4e87f85384dacd
Christian Rodriguez <105233007+ChrisAlexRods@users.noreply.github.com>
2022-10-20 16:19:06 -0400

    Feature 11 complete

--- receipts/urls.py
+++ receipts/urls.py
@@ -1,5 +1,8 @@
-from receipts.views import list_receipt
+from receipts.views import list_receipt, create_receipt
 from django.urls import path, include
 
 
-urlpatterns = [path("", list_receipt, name="home")]
+urlpatterns = [
+    path("", list_receipt, name="home"),
+    path("create/", create_receipt, name="create_receipt"),
+]
--- receipts/views.py
+++ receipts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from receipts.models import ExpenseCategory, Account, Receipt
 from django.contrib.auth.decorators import login_required
+from receipts.forms import ReceiptForm
 
 # Create your views here.
 
@@ -10,3 +11,21 @@ def list_receipt(request):
     receipt_list = Receipt.objects.filter(purchaser=request.user)
     context = {"Receipt": receipt_list}
     return render(request, "receipts/detail.html", context)
+
+
+@login_required
+def create_receipt(request):
+    if request.method == "POST":
+        form = ReceiptForm(request.POST)
+        if form.is_valid():
+            receipt = form.save(False)
+            receipt.purchaser = request.user
+            receipt.save()
+            return redirect("home")
+    else:
+        form = ReceiptForm()
+
+        context = {
+            "form": form,
+        }
+        return render(request, "receipts/create.html", context)
