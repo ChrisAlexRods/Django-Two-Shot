c6fefd23734733732f85628f61125665f680c510
Christian Rodriguez <105233007+ChrisAlexRods@users.noreply.github.com>
2022-10-20 13:47:22 -0400

    Feature 10 complete

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
-from accounts.views import login_user, user_logout
+from accounts.views import login_user, user_logout, signup
 from django.urls import path, include
 
 urlpatterns = [
     path("login/", login_user, name="login"),
     path("logout/", user_logout, name="logout"),
+    path("signup/", signup, name="signup"),
 ]
